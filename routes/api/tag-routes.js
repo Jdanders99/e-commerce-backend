6d522f6fa24547efd48858debfcd4c554ff87827
Jakob A <jdanders1999@gmail.com>
2023-02-02 16:50:08 -0500

    Updated tag-routes to get tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,11 +19,17 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-
+    const tagData = await Tag.findByPk(req.params.id, {
+    // be sure to include its associated Product data
+      include: [{ model: Product, through: ProductTag }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+    }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Product data
 });
 
 router.post('/', async (req, res) => {
