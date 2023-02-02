d308e2bee6ba3f0f3a31868cd5572d8c6dc25026
Jakob A <jdanders1999@gmail.com>
2023-02-02 16:51:23 -0500

    Updated tag-routes to post/create tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,7 +35,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,7 +45,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-
+    
   } catch (err) {
     res.status(500).json(err);
   }
