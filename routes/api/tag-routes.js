3dff6ae13f10e7784ac3a21ad4dd5d742efbb28c
Jakob A <jdanders1999@gmail.com>
2023-02-02 17:58:53 -0500

    Fixed spelling in category and tag routes for destroy/delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,7 +64,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destory({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       }
